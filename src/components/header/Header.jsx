3feaee70c14661537dfd7d0c694c781fe9528a45
Stanislav Vorozhun <stanislav.vorozhun@gmail.com>
2022-12-11 19:15:52 +0100

    add nav menu

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">SV</a>
+        <div className="nav__menu">
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                <i className="uil uil-house-user nav__icon">Home</i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon">About</i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon">Skills</i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon">Services</i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon">Portfolio</i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon">Contact</i>
+              </a>
+            </li>
+          </ul>
+
+          <i class="uil uil-times-circle nav__close"></i>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
