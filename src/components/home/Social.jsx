46b0bf6a0ce1ab81b0abd37c9748bae9dd77a74e
Stanislav Vorozhun <stanislav.vorozhun@gmail.com>
2022-12-12 11:03:27 +0100

    home component adaptive

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://github.com/Stanislav-Vorozhun" className="home__social-icon" target="_blank">
+      <i className="uil uil-github"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/stanislav-vorozhun-aa806a182/" className="home__social-icon" target="_blank">
+      <i className="uil uil-linkedin"></i>
+      </a>
+      <a href="https://t.me/S_Vorozhun" className="home__social-icon" target="_blank">
+      <i className="uil uil-telegram"></i>
+      </a>
+
+    </div>
+  )
+}
+
+export default Social
